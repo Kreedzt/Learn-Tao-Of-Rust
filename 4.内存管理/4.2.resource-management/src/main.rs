feb850800bd5407d24caefdc3f8e0e8dcfd377dd
Kreedzt <zhaozisong1@live.com>
2020-09-13 18:50:04 +0800

    :sparkles: Kreedzt - completed 4.2

--- 4.内存管理/4.2.resource-management/src/main.rs
+++ 4.内存管理/4.2.resource-management/src/main.rs
@@ -1,4 +1,6 @@
 use std::ops::Drop;
+use std::rc::Rc;
+use std::cell::RefCell;
 
 #[derive(Debug)]
 struct S(i32);
@@ -13,6 +15,53 @@ impl Drop for S {
 // 使用 Box<T> 指针分配堆内存
 fn create_box() {
     let box3 = Box::new(3);
+}
+
+// 链表节点指针
+// type NodePtr<T> = Option<Box<Node<T>>>;
+
+// 使用 Rc
+// type NodePtr<T> = Option<Rc<Node<T>>>;
+
+// 使用 RefCell + Rc
+type NodePtr<T> = Option<Rc<RefCell<Node<T>>>>;
+
+// 链表节点 Node 结构体
+struct Node<T> {
+    data: T,
+    next: NodePtr<T>
+}
+
+// 实现 Drop 检测内存释放
+impl<T> Drop for Node<T> {
+    fn drop(&mut self) {
+        println!("Dropping!");
+    }
+}
+
+// 构造悬垂指针
+// fn foo<'a>() -> &'a str {
+//     let a = "hello".to_string();
+//     // error[E0515]: cannot return reference to local variable `a`
+//     // 局部变量 a 在离开 foo 后被销毁. 如果把该引用传递到函数外面就会出现问题
+//     &a
+// }
+
+struct A {
+    a: u32,
+    b: Box<u64>
+}
+
+struct B(i32, f64, char);
+struct N;
+enum E {
+    H(u32),
+    M(Box<u32>)
+}
+
+union U {
+    u: u32,
+    v: u64
 }
 
 fn main() {
@@ -115,4 +164,47 @@ fn main() {
     // 变量遮蔽并不会导致生命周期提前结束
     let x = S(2);
     println!("create shadowing x: {:?}", x);
+
+    // 尝试实现链表
+    // let mut first = Box::new(Node { data: 1, next: None });
+    // let mut second = Box::new(Node { data: 2, next: None });
+    // error[E0382]: assign to part of moved value: `*second`
+    // first.next = Some(second);
+    // 因使用指针 `Box<T>` 发生了值移动, second 变成了未初始化变量
+    // second.next = Some(first);
+
+    // 对链表使用 Rc
+    // let first = Rc::new(Node { data: 1, next: None });
+    // 循环链表
+    // let second = Rc::new(Node { data: 2, next: Some(first.clone())});
+    // 不能对不可变指针修改
+    // first.next = Some(second.clone());
+    // second.next = Some(first.clone());
+
+    // 使用 `RefCell<T>` 保证内部可变
+    let first = Rc::new(RefCell::new(Node {
+        data: 1,
+        next: None
+    }));
+
+    let second = Rc::new(RefCell::new(Node {
+        data: 2,
+        next: Some(first.clone())
+    }));
+
+    // 制造循环引用
+    // 循环引用后析构函数并没有运行, 存在内存泄露
+    first.borrow_mut().next = Some(second.clone());
+    second.borrow_mut().next = Some(first.clone());
+
+    // 悬垂指针
+    // let x = foo();
+
+    // 验证三种符合结构内存布局
+    println!("Box<u32>: {:?}", std::mem::size_of::<Box<u32>>());
+    println!("A: {:?}", std::mem::size_of::<A>());
+    println!("B: {:?}", std::mem::size_of::<B>());
+    println!("N: {:?}", std::mem::size_of::<N>());
+    println!("E: {:?}", std::mem::size_of::<E>());
+    println!("U: {:?}", std::mem::size_of::<U>());
 }
