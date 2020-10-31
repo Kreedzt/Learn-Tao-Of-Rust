5157db4d3c4d3682151621105415c4d0e0f4c125
Kreedzt <zhaozisong1@live.com>
2020-10-31 14:40:55 +0800

    :sparkles: Kreedzt - completed summary to 11.2.2

--- 11.安全并发/11.2.multi-threads-programming/src/main.rs
+++ 11.安全并发/11.2.multi-threads-programming/src/main.rs
@@ -1,7 +1,9 @@
-use std::cell::RefCell;
 use std::panic;
 use std::thread::{current, spawn, Builder, park, sleep};
 use std::time::Duration;
+use std::sync::{Arc};
+use std::rc::Rc;
+use std::cell::RefCell;
 
 fn main() {
     // 11-7 创建线程
@@ -82,19 +84,64 @@ fn main() {
 
 
     // 11-12 `park` 和 `unpark` 函数使用示例
-    let parked_thread = Builder::new()
-        .spawn(|| {
-            println!("Parking thread");
-            // 阻塞
-            park();
+    // let parked_thread = Builder::new()
+    //     .spawn(|| {
+    //         println!("Parking thread");
+    //         // 阻塞
+    //         park();
 
-            println!("Thread unparked");
-        }).unwrap();
+    //         println!("Thread unparked");
+    //     }).unwrap();
 
-    // 生成毫秒为单位的系统超时时间
-    sleep(Duration::from_millis(10));
-    println!("Unpark the thread");
-    // 使阻塞的线程重启
-    parked_thread.thread().unpark();
-    parked_thread.join().unwrap();
+    // // 生成毫秒为单位的系统超时时间
+    // sleep(Duration::from_millis(10));
+    // println!("Unpark the thread");
+    // // 使阻塞的线程重启
+    // parked_thread.thread().unpark();
+    // parked_thread.join().unwrap();
+
+
+    // 11-17 在线程间传递可变字符串
+    // let mut s = "Hello".to_string();
+    // for _ in 0..3 {
+    //     spawn(move || {
+    //         // 违反了 Rust 所有权机制
+    //         // |               ^^^^^^^ value moved into closure here, in previous iteration of loop
+    //         s.push_str(" Rust!");
+    //     });
+    // }
+
+
+    // 11-18 尝试使用 `Rc` 共享所有权
+    // let mut s = Rc::new("Hello".to_string());
+    // for _ in 0..3 {
+    //     let mut s_clone = s.clone();
+    //     spawn(move || {
+    //         // | |         `Rc<String>` cannot be sent between threads safely
+    //         s_clone.push_str(" hello");
+    //     });
+    // }
+
+
+    // 11-19 使用 `Arc` 共享所有权
+    // let s = Arc::new("Hello".to_string());
+    // for _ in 0..3 {
+    //     let s_clone = s.clone();
+    //     spawn(move || {
+    //         s_clone.push_str(" world!");
+    //         // |             ^^^^^^^ cannot borrow as mutable
+    //     });
+    // }
+
+
+    // 11-20 使用 `RefCell` 支持内部可变性.
+    let s = Arc::new(RefCell::new("Hello".to_string()));
+    for _ in 0..3 {
+        let s_clone = s.clone();
+        spawn(move || {
+            // error[E0277]: `RefCell<Strisng>` cannot be shared between threads safely
+            let s_clone = s_clone.borrow_mut();
+            s_clone.push_str(" world!");
+        });
+    }
 }
