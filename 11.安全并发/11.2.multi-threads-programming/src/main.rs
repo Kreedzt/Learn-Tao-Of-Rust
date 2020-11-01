28b26c6db92ab804f39a8542850c9312ce14141d
Kreedzt <zhaozisong1@live.com>
2020-11-01 13:54:07 +0800

    :sparkles: Kreedzt - completed summary to 11.2.3

--- 11.安全并发/11.2.multi-threads-programming/src/main.rs
+++ 11.安全并发/11.2.multi-threads-programming/src/main.rs
@@ -1,9 +1,10 @@
 use std::panic;
 use std::thread::{current, spawn, Builder, park, sleep};
 use std::time::Duration;
-use std::sync::{Arc};
+use std::sync::{Arc, Mutex, RwLock};
 use std::rc::Rc;
 use std::cell::RefCell;
+use rand::prelude::*;
 
 fn main() {
     // 11-7 创建线程
@@ -135,13 +136,132 @@ fn main() {
 
 
     // 11-20 使用 `RefCell` 支持内部可变性.
-    let s = Arc::new(RefCell::new("Hello".to_string()));
-    for _ in 0..3 {
-        let s_clone = s.clone();
+    // let s = Arc::new(RefCell::new("Hello".to_string()));
+    // for _ in 0..3 {
+    //     let s_clone = s.clone();
+    //     spawn(move || {
+    //         // error[E0277]: `RefCell<Strisng>` cannot be shared between threads safely
+    //         let s_clone = s_clone.borrow_mut();
+    //         s_clone.push_str(" world!");
+    //     });
+    // }
+
+
+    // 11-21 使用 `Mutex` 在多线程环境中共享可变变量
+    // let s = Arc::new(Mutex::new("Hello".to_string()));
+    // let mut v = vec![];
+    
+    // for _ in 0..3 {
+    //     let s_clone = s.clone();
+    //     let child = spawn(move || {
+    //         let mut s_clone = s_clone.lock().unwrap();
+    //         // 存在竞态条件, 该操作可能乱序执行.
+    //         s_clone.push_str(" world!");
+    //     });
+    //     v.push(child);
+    // }
+
+    // for child in v {
+    //     child.join().unwrap();
+    // }
+
+
+    // 11-22 "中毒" 示例
+    // let mutex = Arc::new(Mutex::new(1));
+    // let c_mutex = mutex.clone();
+    // let _ = spawn(move || {
+    //     let mut data = c_mutex.lock().unwrap();
+    //     // `data` 为 `MutexGuard<T>` 类型, 该类型实现了 `Deref` 和 `DerefMut`.
+    //     *data = 2;
+    //     panic!("oh no");
+    // }).join();
+
+    // assert_eq!(mutex.is_poisoned(), true);
+
+    // match mutex.lock() {
+    //     Ok(_) => unreachable!(),
+    //     // 因为在子线程内部发生了恐慌, 所以这里只会返回 `Err`
+    //     // 该 `Err` 是 `PoisonError<T>` 类型, 提供了 `get_ref()` 或 `get_mut()` 方法
+    //     Err(p_err) => {
+    //         let data = p_err.get_ref();
+    //         println!("recovered: {}", data);
+    //     }
+    // };
+
+
+    // 11-24 完善抛硬币 `main` 函数
+    let total_flips = Arc::new(Mutex::new(0));
+    let completed = Arc::new(Mutex::new(0));
+    let runs = 8;
+    let target_flips = 10;
+
+    for _ in 0..runs {
+        let total_flips = total_flips.clone();
+        let completed = completed.clone();
+
         spawn(move || {
-            // error[E0277]: `RefCell<Strisng>` cannot be shared between threads safely
-            let s_clone = s_clone.borrow_mut();
-            s_clone.push_str(" world!");
+            flip_simulate(target_flips, total_flips);
+            let mut completed = completed.lock().unwrap();
+            *completed += 1;
         });
     }
+
+    // loop {
+    //     let completed = completed.lock().unwrap();
+    //     if *completed == runs {
+    //         let total_flips = total_flips.lock().unwrap();
+    //         println!("Final average: {}", *total_flips / *completed);
+    //         break;
+    //     }
+    // }
+
+    
+    // 11-26 改为死锁代码
+    // 始终持有互斥锁, 会导致所有子线程阻塞
+    // 间接导致无法更新 `completed` 的值
+    // let completed = completed.lock().unwrap();
+    // while *completed < runs { }
+    // let total_flips = total_flips.lock().unwrap();
+    // println!("Final average: {}", *total_flips / *completed);
+
+
+    // 11-27 读写锁示例
+    let lock = RwLock::new(5);
+    {
+        let r1 = lock.read().unwrap();
+        let r2 = lock.read().unwrap();
+        assert_eq!(*r1, 5);
+        assert_eq!(*r2, 5)
+    }
+    {
+        let mut w = lock.write().unwrap();
+        *w += 1;
+        assert_eq!(*w, 6);
+    }
+}
+
+
+// 11-23 模拟掷硬币函数
+fn flip_simulate(target_flips: u64, total_flips: Arc<Mutex<u64>>) {
+    // 正面次数
+    let mut countinue_positive = 0;
+    // 掷硬币次数
+    let mut iter_counts = 0;
+
+    while countinue_positive < target_flips {
+        iter_counts += 1;
+        // 获取随机的 `bool` 类型
+        let pro_or_con = rand::random();
+
+        // 推断出随机函数值的类型
+        if pro_or_con {
+            countinue_positive += 1;
+        } else {
+            countinue_positive = 0;
+        }
+    }
+
+    println!("iter_counts: {}", iter_counts);
+    let mut total_flips = total_flips.lock().unwrap();
+    *total_flips += iter_counts;
 }
